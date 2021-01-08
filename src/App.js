df98a3a2e8448593e529cf5513c5962826d5be4a
Bruno Prieto González <brunoprietog@outlook.cl>
2021-01-08 18:01:47 -0300

    Prettier and eslint

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { Stock, Portfolio } from './profit2';
-import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
-import './App.css';
-import NavBar from './components/NavBar';
-import logo from './logo.svg';
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { Stock, Portfolio } from "./profit2";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import logo from "./logo.svg";
 
 function App() {
   //Redux
@@ -35,15 +35,15 @@ function App() {
 
   const index = 11;
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [stock, setStock] = useState('');
-  const [date, setDate] = useState('');
+  const [stock, setStock] = useState("");
+  const [date, setDate] = useState("");
   const [price, setPrice] = useState();
   const addStock = (event) => {
     event.preventDefault();
     var stockObject = new Stock(stock);
     stockObject.setPricePerDate(date, price);
     dispatch({
-      type: 'ADD',
+      type: "ADD",
       stock: stockObject,
     });
     onClose();
@@ -52,43 +52,56 @@ function App() {
     <div className="App">
       <NavBar />
       <header className="App-header">
-        <button onClick={() => changeLanguage('es')}>ESPAÑOL</button>
-        <button onClick={() => changeLanguage('en')}>INGLES</button>
+        <button onClick={() => changeLanguage("es")}>ESPAÑOL</button>
+        <button onClick={() => changeLanguage("en")}>INGLES</button>
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>{t('bienvenido')}</h2>
+        <h2>{t("bienvenido")}</h2>
       </header>
       <main>
-        <p>{t('portfolio')}</p>
-        {stocksState.stocks.length > 0 && stocksState.stocks.map((item) => <p>{item.getCompany()}</p>)}
-        <Button onClick={onOpen}>{t('addOrModifyStock')}</Button>
+        <p>{t("portfolio")}</p>
+        {stocksState.stocks.length > 0 &&
+          stocksState.stocks.map((item) => <p>{item.getCompany()}</p>)}
+        <Button onClick={onOpen}>{t("addOrModifyStock")}</Button>
 
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
-            <ModalHeader>{t('addOrModifyStock')}</ModalHeader>
+            <ModalHeader>{t("addOrModifyStock")}</ModalHeader>
             <ModalCloseButton />
             <form onSubmit={addStock}>
               <ModalBody>
                 <FormControl id="stock">
-                  <FormLabel>{t('stock')}</FormLabel>
-                  <Input type="text" name="stock" onChange={(event) => setStock(event.currentTarget.value)} />
-                  <FormHelperText>{t('formHelperStock')}</FormHelperText>
+                  <FormLabel>{t("stock")}</FormLabel>
+                  <Input
+                    type="text"
+                    name="stock"
+                    onChange={(event) => setStock(event.currentTarget.value)}
+                  />
+                  <FormHelperText>{t("formHelperStock")}</FormHelperText>
                 </FormControl>
                 <FormControl id="date">
-                  <FormLabel>{t('date')}</FormLabel>
-                  <Input type="date" name="date" onChange={(event) => setDate(event.currentTarget.value)} />
-                  <FormHelperText>{t('formHelperDate')}</FormHelperText>
+                  <FormLabel>{t("date")}</FormLabel>
+                  <Input
+                    type="date"
+                    name="date"
+                    onChange={(event) => setDate(event.currentTarget.value)}
+                  />
+                  <FormHelperText>{t("formHelperDate")}</FormHelperText>
                 </FormControl>
                 <FormControl id="price">
-                  <FormLabel>{t('price')}</FormLabel>
-                  <Input type="number" name="price" onChange={(event) => setPrice(event.currentTarget.value)} />
-                  <FormHelperText>{t('formHelperPrice')}</FormHelperText>
+                  <FormLabel>{t("price")}</FormLabel>
+                  <Input
+                    type="number"
+                    name="price"
+                    onChange={(event) => setPrice(event.currentTarget.value)}
+                  />
+                  <FormHelperText>{t("formHelperPrice")}</FormHelperText>
                 </FormControl>
               </ModalBody>
 
               <ModalFooter>
                 <Button colorScheme="blue" mr={3} onClick={onClose}>
-                  {t('close')}
+                  {t("close")}
                 </Button>
                 <Button variant="ghost" type="submit">
                   Secondary Action
